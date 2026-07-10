89493cbcfea84afe780a50a157b1c0e806b96d65
DEADBYTE <termay10000@gmail.com>
2026-07-11 04:33:32 +0700

    add evidence report interface

---
+++ include/deadflash/report.h
@@ -0,0 +1,20 @@
+#ifndef DEADFLASH_REPORT_H
+#define DEADFLASH_REPORT_H
+
+#include "deadflash/fat32.h"
+
+typedef struct df_report_context {
+    const char *operation;
+    const char *source_path;
+    const char *target_path;
+    const df_target_info *target;
+    const df_write_options *write_options;
+    const df_write_result *write_result;
+    const df_fat32_layout *fat32_layout;
+    df_status status;
+    const df_error *error;
+} df_report_context;
+
+df_status df_write_json_report(const char *path, const df_report_context *context, df_error *error);
+
+#endif
